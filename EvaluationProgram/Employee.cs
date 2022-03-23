f7b662ce1f07aa2975766172ae64e96d828256f4
Nagarjuna2001 <99009279+Nagarjuna2001@users.noreply.github.com>
2022-03-23 11:22:13 +0530

    Add files via upload

---
+++ EvaluationProgram/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvaluationProgram
+{
+    public abstract class Employee
+    {
+        public Employee() { }
+
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LasttName { get; set; }
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LasttName;
+        }
+
+        public abstract void AccessPermissionsToTeamsChannel();
+    }
+}
---
+++ EvaluationProgram/NormalEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvaluationProgram
+{
+    public class NormalEmployee : Employee
+    {
+        public NormalEmployee() { }
+
+        public override void AccessPermissionsToTeamsChannel()
+        {
+            Console.WriteLine("Contains logic which has access permission to add or delete new post in the channel");
+
+            //Does not have permissions (logic) to add and remove members from the channel
+        }
+        
+    }
+}
