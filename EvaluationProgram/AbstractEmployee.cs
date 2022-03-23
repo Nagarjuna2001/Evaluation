358319466f9aee335476f8be5c259edad6a2fb19
Nagarjuna2001 <99009279+Nagarjuna2001@users.noreply.github.com>
2022-03-23 14:13:13 +0530

    Add files via upload

---
+++ EvaluationProgram/AbstractEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvaluationProgram
+{
+    public abstract class AbstractEmployee
+    {
+        public AbstractEmployee() { }
+
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string OrganisationName = "NCR Corporation";
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public abstract void AccessPermissionsToTeamsChannel();
+    }
+}
---
+++ EvaluationProgram/MainExecutor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EvaluationProgram
+{
+    public class MainExecutor
+    {
+        static void Main(string[] args)
+        {
+            AdminEmployee adminEmployee = new AdminEmployee();
+            adminEmployee.ID = 15;
+            adminEmployee.FirstName = "Steve";
+            adminEmployee.LastName = "Smith";
+
+            Console.WriteLine("Organistaion Name : {0}", adminEmployee.OrganisationName);
+            Console.WriteLine("Employee's Name : " + adminEmployee.GetFullName());
+            adminEmployee.AccessToGrantLeaves();
+            adminEmployee.AccessToUpdateInventory();
+            adminEmployee.AccessPermissionsToTeamsChannel();
+
+            Console.WriteLine("\n");
+
+            NormalEmployee normalEmployee = new NormalEmployee();
+            normalEmployee.ID = 20;
+            normalEmployee.FirstName = "Michael";
+            normalEmployee.LastName = "Jordan";
+
+            Console.WriteLine("Organistaion Name : {0}", adminEmployee.OrganisationName);
+            Console.WriteLine("Employee's Name : " + normalEmployee.GetFullName());
+            normalEmployee.AccessPermissionsToTeamsChannel();
+        }
+    }
+}
