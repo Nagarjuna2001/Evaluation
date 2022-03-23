0d4bd9544ff3021036040f562bdcbf122c6d06a3
Nagarjuna2001 <99009279+Nagarjuna2001@users.noreply.github.com>
2022-03-23 11:40:55 +0530

    Add files via upload

---
+++ Collection/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection
+{
+    public class Employee
+    {
+        public Employee() { }
+
+        public string ID { get; set; }
+        public int salary { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Collection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee firstEmployee = new Employee { ID = "100", Name = "Smith" ,salary = 5000};
+            Employee secondEmployee = new Employee { ID = "101", Name = "Mark", salary = 4000 };
+            Employee thirdEmployee = new Employee { ID = "101", Name = "Alice", salary = 6000 };
+
+            Dictionary<string, Employee> fetchEmployeeDetails = new Dictionary<string, Employee>();
+            fetchEmployeeDetails.Add(firstEmployee.ID, firstEmployee);
+            fetchEmployeeDetails.Add(secondEmployee.ID, secondEmployee);
+
+            
+
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(firstEmployee);
+            employeeList.Add(secondEmployee);
+            employeeList.Add(thirdEmployee);
+
+            List<Employee> employeesWithSalaryAboveFiveThousand = employeeList.FindAll(employee => employee.salary >= 5000);
+
+            foreach(Employee employee in employeesWithSalaryAboveFiveThousand)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+        }
+    }
+}
